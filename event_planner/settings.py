be7eb1702e624a1fb1a7aa6b617b4ff92e8584d5
siboniseni <siboniseni.kasa@outlook.com>
2025-01-08 12:29:57 +0200

    Prepare Django app for Heroku deployment

--- event_planner/settings.py
+++ event_planner/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-mms2&o-@=-(xf&uue#hrvtwz#y-75g)!ghad@-vjldpa_6=wje
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['eventplanner.com', 'www.eventplanner.com', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
